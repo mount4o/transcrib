5eb24a0db3a9d1acf2632231d94be0ec8c3237f0
Vasilen Alexandrov <vasilen.alexandrov@endurosat.com>
2024-05-16 11:43:18 +0300

    Attemtping to fix Mix content issue on Firefox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FileUpload from "../components/FileUpload";
 import TranscribedText from "../components/TranscribedText"
 import TranslatedText from "../components/TranslatedText"
 import FlexContainer from "../components/FlexContainer"
+import Head from 'next/head'
 import { useState } from "react";
 
 export default function Home() {
@@ -17,6 +18,9 @@ export default function Home() {
 
   return (
       <div className="h-screen flex flex-col items-center justify-center gap-2">
+        <Head>
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        </Head>
         {!transcribedText && <FileUpload onResponse={onFileUploadSuccess} /> }
         {transcribedText && <TranscribedText textContent={transcribedText} />}
         {transcribedText && <TranslatedText textForTranslation={transcribedText} />}
