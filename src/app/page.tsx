d82704743bf20136657a38fde3d53bc36efbbbf3
Vasilen Alexandrov <vasilen.alexandrov@endurosat.com>
2024-05-17 13:43:30 +0300

    Removed gtag npm module and added gtag manually to <Head>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ import FileUpload from "../components/FileUpload";
 import TranscribedText from "../components/TranscribedText"
 import TranslatedText from "../components/TranslatedText"
 import Head from 'next/head'
+import Script from 'next/script';
 import { useState, useEffect } from "react";
 import Hotjar from '@hotjar/browser';
-import TagManager from "react-gtm-module";
-
 
 export default function Home() {
   const [transcribedText, setTranscribedText] = useState("");
@@ -16,9 +15,6 @@ export default function Home() {
     const siteId = 4985123;
     const hotjarVersion = 6;
     Hotjar.init(siteId, hotjarVersion);
-
-    const gtmId = "AW-16570343427";
-    TagManager.initialize({ gtmId });
   }, []);
 
   function onFileUploadSuccess(resultData : any) {
@@ -28,13 +24,22 @@ export default function Home() {
   }
 
   return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16570343427"></Script>
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-16570343427');
+        `}
+      </Script>
       <div className="h-screen flex flex-col items-center justify-center gap-2">
-        <Head>
-          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-        </Head>
         {!transcribedText && <FileUpload onResponse={onFileUploadSuccess} /> }
         {transcribedText && <TranscribedText textContent={transcribedText} />}
         {transcribedText && <TranslatedText textForTranslation={transcribedText} />}
       </div>
+    </>
   );
 }
