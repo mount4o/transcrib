76542231d0ee4d258ec1cf855697ddef23f797e1
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2024-11-09 10:36:48 +0200

    Enabling CORS on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Match all routes
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Change '*' to your specific domain if needed
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
