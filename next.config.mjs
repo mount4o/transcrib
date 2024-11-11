4ad321f8722a9851df3543e97b82ea35aa304fa0
Vasilen Alexandrov <vasilen.alexandrov@gmail.com>
2024-11-11 12:27:21 +0200

    CORS disable attempt 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
+  async rewrites() {
     return [
       {
-        // Match all routes
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Change '*' to your specific domain if needed
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" },
-        ],
+        source: '/api/:path*', // When requests are made to /api/<path>, they will be proxied
+        destination: 'https://your-remote-api.com/:path*',
       },
     ];
   },
